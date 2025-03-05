30534c852a32428bb9c56f1e9553afc5557e505a
Joan Coronado <joan15corza@gmail.com>
2025-03-05 11:11:26 -0600

    Updates

--- _app/immutable/entry/start.CTwM8TOl.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/o1gLFc-2.js";export{o as load_css,r as start};
--- _app/immutable/nodes/2.BgyVaTne.js
+++
@@ -1,1 +0,0 @@
-import{t as o,a as t}from"../chunks/dQmbahsI.js";import"../chunks/AhgGfyHc.js";var p=o("<h1>Bienvenidos a Azalea Web</h1>");function i(a){var e=p();t(a,e)}export{i as component};
