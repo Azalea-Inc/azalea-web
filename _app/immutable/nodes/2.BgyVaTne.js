6f65e0653629f40805d2d121ba92b351ba16f6e4
Joan Coronado <joan15corza@gmail.com>
2025-03-05 11:00:16 -0600

    Updates

---
+++ _app/immutable/nodes/2.BgyVaTne.js
@@ -0,0 +1,1 @@
+import{t as o,a as t}from"../chunks/dQmbahsI.js";import"../chunks/AhgGfyHc.js";var p=o("<h1>Bienvenidos a Azalea Web</h1>");function i(a){var e=p();t(a,e)}export{i as component};
--- _app/immutable/nodes/2.C0q1dJvB.js
+++
@@ -1,1 +0,0 @@
-import{t as e,a as t}from"../chunks/dQmbahsI.js";import"../chunks/AhgGfyHc.js";var p=e("<h1>Bienvenidos a Azalea Blog</h1>");function i(a){var o=p();t(a,o)}export{i as component};
